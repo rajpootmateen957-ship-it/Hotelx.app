11a23401b413de24fb84a0f4b9dd3281d2310b33
Mateen Ahmed <tumhara-github-email@gmail.com>
2026-07-19 03:35:12 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import Navbar from "./components/Layout/navbar";
-import Hero from "./components/section/hero";
-import Official from "./components/section/official";
-import Counter from "./components/section/counter";
-import AiSection from "./components/section/aisection";
-import Testimonial from "./components/section/testimonial";  
-import Cta from "./components/section/cta";
-import Competitor from "./components/section/competitor";
-import DashboardMock from "./components/section/feature/page";
-import Pricing from "./components/section/pricing";
-import Faq from "./components/section/faq";
-import Contact from "./components/section/contact";
-import Footer from "./components/section/footer";
-
+import Home from "./page/home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Official />
-      <Counter />
-      <AiSection />
-<DashboardMock/>
-
-      <Testimonial />
-      <Cta />
-      <Competitor />
-      <Pricing />
-      <Faq />
-      <Contact />
-      <Footer />
-       
-
-     
-      
+      <Home />
     </>
   );
 }
